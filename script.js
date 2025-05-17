9bc24db6bccc281412888a1b3e7dca351df79dbc
abiruba <abimathics06@gmail>
2025-05-17 16:01:09 -0400

    java script add

---
+++ script.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = 15;
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a != b)
+console.log(a % b)
+console.log(a <= b)
+console.log(a >= b)
+console.log(b == a)
+console.log(b * a)
+
+function mulit(a, b) {
+    console.log("a*b", a * b)
+}
+mulit(50, 45)
+mulit(30, 46)
+mulit(24, 67)
