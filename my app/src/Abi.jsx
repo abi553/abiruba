c2bbf27de30af3c2b6c203df93ec711542398119
abiruba <abimathics06@gmail>
2025-05-22 10:24:52 -0400

    third commit

---
+++ my app/src/Abi.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import image from './image/cat.jpg'
+function Abi() {
+  return (
+    <>
+    <div>sri</div>
+    <img src={image} controls/>
+    </>
+
+  )
+}
+
+export default Abi
---
+++ my app/src/Form.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <>
+    <div> 
+        <label>name:</label>
+        <input
+        type="text"
+        name="name"/>
+    </div>
+     <div> 
+        <label>email:</label>
+        <input
+        type="email"
+        name="email"/>
+    </div>
+    <div>
+        <label>class:</label>
+        <input
+        type="text"
+        name="name"/>
+    </div>
+    <div>
+        <label>age:</label>
+        <input
+        type="text"
+        name="name"/>
+    </div>
+    </>
+    
+  )
+}
+
+export default Form
